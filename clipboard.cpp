1a44f540cc50f4ae7db15ce7c51265131085864d
ghost_him <70733461+ghost-him@users.noreply.github.com>
2024-06-11 22:49:55 +0800

    Add files via upload

--- clipboard.cpp
+++ clipboard.cpp
@@ -5,40 +5,40 @@
 #include <sstream>
 #include <thread>
 
+#define WM_TRAYICON (WM_USER + 1)
+#define ID_TRAY_APP_ICON 1001
+#define ID_TRAY_EXIT 1002
+
 // 当前是否停止了
 bool isStop = false;
 // 总共按下的次数
 int pressCount = 0;
+NOTIFYICONDATA nid;
+HMENU hMenu;
 
-
+// 设置剪切板文本内容
 void SetStringToClipboard(const std::string& text) {
-
-    // 打开剪切板
     if (!OpenClipboard(nullptr)) {
         std::cerr << "无法打开剪切板" << std::endl;
         return;
     }
 
-    // 清空剪切板
     if (!EmptyClipboard()) {
         std::cerr << "无法清空剪切板" << std::endl;
         CloseClipboard();
         return;
     }
 
-    // 为文本分配全局内存
-    HGLOBAL hGlobal = GlobalAlloc(GMEM_MOVEABLE, std::size(text) + 1);
+    HGLOBAL hGlobal = GlobalAlloc(GMEM_MOVEABLE, text.size() + 1);
     if (hGlobal == nullptr) {
         std::cerr << "无法分配内存" << std::endl;
         CloseClipboard();
         return;
     }
 
-    // 将文本复制到全局内存
-    memcpy(GlobalLock(hGlobal), text.data(), std::size(text) + 1);
+    memcpy(GlobalLock(hGlobal), text.data(), text.size() + 1);
     GlobalUnlock(hGlobal);
 
-    // 将全局内存放到剪切板上
     if (SetClipboardData(CF_TEXT, hGlobal) == nullptr) {
         std::cerr << "无法设置剪切板数据" << std::endl;
         GlobalFree(hGlobal);
@@ -46,7 +46,6 @@ void SetStringToClipboard(const std::string& text) {
         return;
     }
 
-    // 关闭剪切板
     CloseClipboard();
 }
 
@@ -71,8 +70,6 @@ std::string GetClipboardText() {
     return text;
 }
 
-
-
 // 键盘钩子回调函数
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode == HC_ACTION) {
@@ -80,7 +77,6 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
         if (wParam == WM_KEYDOWN) {
             if ((GetAsyncKeyState(VK_CONTROL) & 0x8000) && pKeyboard->vkCode == 'C') {
                 if (!isStop) {
-                    //按下后开一个线程，过100ms进行更新操作
                     std::thread the([]() {
                         std::this_thread::sleep_for(std::chrono::milliseconds(100));
                         std::string clipboardText = GetClipboardText();
@@ -88,7 +84,6 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
                             std::cout << "已检测到剪切板的内容，一共复制了：" << ++pressCount << "次" << std::endl;
                             std::stringstream ss(clipboardText);
                             for (auto& i : clipboardText) {
-                                // 13:回车键,10:换行键
                                 if (i == 13 || i == 10) {
                                     i = ' ';
                                 }
@@ -112,7 +107,6 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode == HC_ACTION) {
         KBDLLHOOKSTRUCT* pKeyboard = (KBDLLHOOKSTRUCT*)lParam;
 
-        // 检查是否按下了 Ctrl + Shift + Alt + P
         if ((GetAsyncKeyState(VK_CONTROL) & 0x8000) &&
             (GetAsyncKeyState(VK_SHIFT) & 0x8000) &&
             (GetAsyncKeyState(VK_MENU) & 0x8000) &&
@@ -125,22 +119,80 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     return CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
 
-int main()
+// 窗口过程函数
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_TRAYICON:
+        if (lParam == WM_RBUTTONDOWN) {
+            POINT p;
+            GetCursorPos(&p);
+            SetForegroundWindow(hwnd);
+            TrackPopupMenu(hMenu, TPM_BOTTOMALIGN | TPM_LEFTALIGN, p.x, p.y, 0, hwnd, NULL);
+        }
+        else if (lParam == WM_MOUSEHOVER) {
+            std::wstring tooltip = isStop ? L"程序已暂停" : L"程序运行中";
+            for (int i = 0; i < tooltip.size(); i++) {
+                nid.szTip[i] = tooltip[i];
+            }
+            Shell_NotifyIcon(NIM_MODIFY, &nid);
+        }
+        break;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == ID_TRAY_EXIT) {
+            Shell_NotifyIcon(NIM_DELETE, &nid);
+            PostQuitMessage(0);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR     lpCmdLine,
+    int       nCmdShow)
 {
-    std::cout << "Ciallo～(∠·ω< )⌒★" << std::endl;
-    std::cout << "作者：https://github.com/ghost-him" << std::endl;
-    std::cout << "开始监听 ctrl + c " << std::endl;
-    // 安装全局键盘钩子
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"ClipboardModifierClass";
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindow(L"ClipboardModifierClass", L"Clipboard Modifier", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 200, NULL, NULL, hInstance, NULL);
+
+    hMenu = CreatePopupMenu();
+    AppendMenu(hMenu, MF_STRING, ID_TRAY_EXIT, L"退出");
+
+    nid.cbSize = sizeof(NOTIFYICONDATA);
+    nid.hWnd = hwnd;
+    nid.uID = ID_TRAY_APP_ICON;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uCallbackMessage = WM_TRAYICON;
+    nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    std::wstring str = L"程序运行中";
+    for (int i = 0; i < str.size(); i++) {
+        nid.szTip[i] = str[i];
+    }
+    Shell_NotifyIcon(NIM_ADD, &nid);
+
     HHOOK hhkLowLevelKybd = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
     HHOOK control = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
-    // 消息循环
+
     MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0)) {
+    while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-    // 卸载钩子
+
     UnhookWindowsHookEx(hhkLowLevelKybd);
     UnhookWindowsHookEx(control);
+    Shell_NotifyIcon(NIM_DELETE, &nid);
+    DestroyMenu(hMenu);
+
     return 0;
 }
